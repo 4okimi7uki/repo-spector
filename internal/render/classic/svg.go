05e73063a996909b41021dac156d7855faf8b555
4okimi7uki <bv16001@shibaura-it.ac.jp>
2026-05-04 23:23:37 +0900

    style: add animation and refactoring

--- internal/render/classic/svg.go
+++ internal/render/classic/svg.go
@@ -11,25 +11,8 @@ func defaultSVGConfig() models.SvgConfig {
 	return models.SvgConfig{
 		BarWidth:  370,
 		BarHeight: 10,
-
-		Width:  410,
-		Height: 255,
-
-		// TitleX: 33,
-		// TitleY: 40,
-
-		// TopLangX: 135,
-		// TopLangY: 80,
-
-		// TopPercentX: 330,
-		// TopPercentY: 70,
-
-		// RowStartY: 150,
-		// RowGap:    30,
-
-		// OverviewShiftY: 18,
-
-		// MaxBarWidth: 200,
+		Width:     410,
+		Height:    255,
 	}
 }
 
@@ -51,23 +34,17 @@ func BuildSVG(aggregate models.LangStatWithTotal, repo models.RepositoryCountAnd
 	// style
 	renderStyles(&sb)
 
+	//card
 	fmt.Fprintf(&sb, `<rect id="border" x="0" y="0" width="%d" height="%d" fill='#3D444D' rx="15" ry="15" />`+"\n", cfg.Width, cfg.Height)
 	fmt.Fprintf(&sb, `<rect id="bg" x="1" y="1" width="%d" height="%d" rx="14.5" ry="14.5" />`+"\n\n", cfg.Width-2, cfg.Height-2)
 
 	// title
 	sb.WriteString(`<text id="title" x="20" y="40" font-size="18" font-weight="bold" fill='#fff' font-family="system-ui, -apple-system, sans-serif">Top Languages</text>` + "\n")
 
-	// avatar
-	if repo.AvatarUrl != "" {
-		fmt.Fprint(&sb, `  <defs><clipPath id="avatarClip"><circle cx="19" cy="19" r="14" /></clipPath></defs>`+"\n")
-		fmt.Fprintf(&sb, `  <image href="%s" x="5" y="5" width="28" height="28" clip-path="url(#avatarClip)" />`+"\n", repo.AvatarUrl)
-	}
-	sb.WriteString("\n")
-
 	fmt.Fprintf(&sb, `<defs>
-    <clipPath id="roundedClip">
-        <rect id="bar_back" x="20" y="60" width="%d" height="%d" rx="5" ry="5">
-        </rect>
+	<clipPath id="roundedClip">
+    	<rect id="bar_back" x="20" y="60" width="%d" height="%d" rx="5" ry="5">
+     	</rect>
     </clipPath>
 </defs>`+"\n\n", cfg.BarWidth, cfg.BarHeight)
 
